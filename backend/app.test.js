0cb696cb3876a59174dc6da1f785e2a4aec065a5
Corrin <corrin@Corrins-MacBook-Air.local>
2026-07-08 16:35:32 +0800

    fix: resolve typo in app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('./app');
 
-describe('GET /api/status', () => {
+describe('GET /api/ready', () => {
   it('should return a 200 status and ok message', async () => {
     // Supertest makes a mock HTTP request to your Express app
     const response = await request(app).get('/api/status');
