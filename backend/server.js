b2ca53093dc7a54bbc936b8117cbabbdc19bae90
Corrin <corrin@Corrins-MacBook-Air.local>
2026-07-08 17:01:46 +0800

    chore: add graceful shutdown logic with db and redis

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,53 @@ const app = require('./app');
 const connectMongoDB = require('./config/db');
 const { connectRedis } = require('./config/redis');
 
-// Initialize Database and Redis Connections
-connectMongoDB();
-connectRedis();
-
-
 // Start the server
 const port = process.env.PORT || 5001;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+    
+  // Initialize Database and Redis Connections
+  connectMongoDB();
+  connectRedis();
+});
+
+
+// Stop server with Graceful Shutdown
+const gracefulShutdown = async (signal) => {
+  console.log(`\n ${signal} received. Starting graceful shutdown...`);
+
+  // Stop accepting NEW connections (Keep the existing ones open)
+  server.close(() => {
+    console.log('HTTP server closed. No longer accepting new requests.');
+  });
+
+  try {
+    // Close MongoDB Connection
+    const mongoose = require('mongoose');
+    if (mongoose.connection.readyState !== 0) {
+      await mongoose.connection.close();
+      console.log('MongoDB connection closed.');
+    }
+
+    // Close Redis Connection
+    const { redisClient } = require('./config/redis'); 
+    if (redisClient && redisClient.isOpen) {
+      await redisClient.quit(); // .quit() waits for pending commands to finish. .disconnect() does not.
+      console.log('Redis connection closed.');
+    }
+
+    console.log('Graceful shutdown completed successfully.');
+    process.exit(0); // Exit with success code
+
+  } catch (error) {
+    console.error('Error during graceful shutdown:', error);
+    process.exit(1); // Exit with failure code
+  }
+};
+
+// Listen for SIGTERM (Sent by Docker)
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+
+// Listen for SIGINT (Ctrl+C)
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
--- frontend/app/ready/page.tsx
+++ frontend/app/ready/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   useEffect(() => {
     // Fetch data from Express backend
-    fetch('/api/status')
+    fetch('/api/ready')
       .then((res) => {
         if (!res.ok) throw new Error("Network response was not ok");
         return res.json();
