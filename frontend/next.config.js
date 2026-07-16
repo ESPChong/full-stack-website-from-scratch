b75764670f5dbb08fc14719a5bad9a5f6392898f
Corrin <corrin@Corrins-MacBook-Air.local>
2026-07-16 23:58:16 +0800

    chore: trigger workflow rerun #3

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
-import path from 'path';
+const path = require('path');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   outputFileTracingRoot: path.join(__dirname, '..'),
-
   turbopack: {
     root: __dirname,
   },
