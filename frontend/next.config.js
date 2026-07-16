1febf89aa260ab29383b7b0b9bcd4adcff5ddc40
Corrin <corrin@Corrins-MacBook-Air.local>
2026-07-16 23:59:32 +0800

    chore: trigger workflow rerun #4

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Recreate __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,4 +14,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig;
